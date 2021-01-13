4a27e05f455b4ba1ba29ed50c87e4b03222c3763
Vasil Shaddix <vasilshaddixx@gmail.com>
2021-01-13 20:34:03 +0200

    define routes for user REST api points

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,26 @@
 class UserController < ApplicationController
+  # Route handler for GET: /user
+  def index
+    render json: { message: 'get all users' }, status: 200
+  end
+
+  # Route handler for POST: /user
+  def create
+    render json: { message: 'create a user' }, status: 200
+  end
+
+  # Route handler for PUT/PATCH: /user/:id
+  def update
+    render json: { message: 'update user' }, status: 200
+  end
+
+  # Route handler for DELETE: /user/:id
+  def destroy
+    render json: { message: 'delete user' }, status: 200
+  end
+
+  # Route handler for GET: /user/:id
+  def show
+    render json: { message: 'get single user' }, status: 200
+  end
 end
