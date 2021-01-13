0f255d7857b068366c3bae5bc8a932ea949f0202
Vasil Shaddix <vasilshaddixx@gmail.com>
2021-01-14 01:29:08 +0200

    create authenticate method (login)

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,19 @@
+class AuthenticationController < ApplicationController
+  def authenticate
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session = UserSessionManager.instance.create(user)
+
+      response.headers['Authorization'] = session.hash_representation
+    else
+      render json: { data: [], message: "No user found" }, status: 422
+    end
+  end
+
+  # test method
+  def ping
+    session = UserSessionManager.instance.setup_session_by_hash(request.headers['Authorization'])
+
+    render json: { data: session }, status: 200
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :user
+  post 'authenticate', to: 'authentication#authenticate'
+  post 'ping', to: 'authentication#ping'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
